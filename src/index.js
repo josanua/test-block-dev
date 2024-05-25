d68ae28e77accd09aa634bbb9438f0da95a368c5
Josanu Andrei <andrei@quadra.md>
2024-05-25 13:47:12 +0300

    Add @wordpress/block-editor package and implement them to use blocks props and styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-// let registerBlockType = wp.blocks.registerBlockType;
-import {registerBlockType} from "@wordpress/blocks"
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps } from '@wordpress/block-editor';
 
-registerBlockType("blocks-course/firstblock", {
-    edit: function () {
-        return <p className="blocks-edit">Edit</p>;
-    },
-    save: function () {
-        return <p className="blocks-save">Save</p>;
-    },
-})
+registerBlockType( 'blocks-course/firstblock', {
+	edit: function () {
+		const blockProps = useBlockProps();
+		return <p {...blockProps}>Edit JSX</p>;
+	},
+	save: function () {
+		const blockProps = useBlockProps.save();
+		return <p {...blockProps}>Save</p>;
+	},
+} );
