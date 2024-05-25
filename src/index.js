e4a7011307ac86a3d9cca9112db65d80f8b0913d
Josanu Andrei <andrei@quadra.md>
2024-05-25 13:07:10 +0300

    Change files to work from src dir.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// let registerBlockType = wp.blocks.registerBlockType;
+import {registerBlockType} from "@wordpress/blocks"
+
+registerBlockType("blocks-course/firstblock", {
+    edit: function () {
+        return <p className="blocks-edit">Edit</p>;
+    },
+    save: function () {
+        return <p className="blocks-save">Save</p>;
+    },
+})
